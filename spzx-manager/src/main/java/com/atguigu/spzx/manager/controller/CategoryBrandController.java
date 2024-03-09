479435f2e08166853521a2418c6de88ab5356444
zhu <779430226@qq.com>
2024-03-09 13:55:42 +0800

    品牌的分页查询控制

---
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/controller/CategoryBrandController.java
@@ -0,0 +1,32 @@
+package com.atguigu.spzx.manager.controller;
+
+import com.atguigu.spzx.manager.service.CategoryBrandService;
+import com.atguigu.spzx.model.dto.product.CategoryBrandDto;
+import com.atguigu.spzx.model.entity.product.CategoryBrand;
+import com.atguigu.spzx.model.vo.common.Result;
+import com.atguigu.spzx.model.vo.common.ResultCodeEnum;
+import com.github.pagehelper.PageInfo;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "分类品牌列表")
+@RestController
+@RequestMapping(value = "/admin/product/categoryBrand")
+
+public class CategoryBrandController {
+
+    @Autowired
+    private CategoryBrandService categoryBrandService ;
+
+    @Operation(summary = "分页查询")
+    @GetMapping("/{page}/{limit}")
+    public Result<PageInfo<CategoryBrand>> findByPage(@PathVariable Integer page, @PathVariable Integer limit, CategoryBrandDto CategoryBrandDto) {
+        PageInfo<CategoryBrand> pageInfo = categoryBrandService.findByPage(page, limit, CategoryBrandDto);
+        return Result.build(pageInfo , ResultCodeEnum.SUCCESS) ;
+    }
+}
