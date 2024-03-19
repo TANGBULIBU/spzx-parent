acb8652c82c605c7b6db15930f7950f1fb91f724
zhu <779430226@qq.com>
2024-03-19 19:57:32 +0800

    添加网关服务 通过gateway对相关内容进行访问 可以暂时不用添加@CrossOrigin注解在其中

--- spzx-service/service-product/src/main/java/com/atguigu/spzx/product/controller/CategoryController.java
+++ spzx-service/service-product/src/main/java/com/atguigu/spzx/product/controller/CategoryController.java
@@ -7,17 +7,18 @@ import com.atguigu.spzx.product.service.CategoryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin
+//@CrossOrigin
 @Tag(name = "分类接口管理")
 @RestController
 @RequestMapping("/api/product/category")
+
+//告诉编译器忽略指定的警告，不用在编译完成后出现警告信息。
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class CategoryController {
     @Autowired
--- spzx-service/service-product/src/main/java/com/atguigu/spzx/product/controller/IndexController.java
+++ spzx-service/service-product/src/main/java/com/atguigu/spzx/product/controller/IndexController.java
@@ -10,7 +10,6 @@ import com.atguigu.spzx.product.service.ProductSkuService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 //解决跨域
-@CrossOrigin
+//@CrossOrigin
 @Tag(name = "首页接口管理")
 @RestController
 @RequestMapping(value="/api/product/index")
