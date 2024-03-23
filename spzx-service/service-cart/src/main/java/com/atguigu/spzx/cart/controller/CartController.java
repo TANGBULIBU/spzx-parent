334ab4f21c9afe6d6a533896cc5130370f7d8062
zhu <779430226@qq.com>
2024-03-23 08:32:11 +0800

    完成购物车模块的所有功能 购物车的列表查询 购物车的删除 购物车的选中信息的更新，购物车的商品全选还有购物车的内容清空的功能

--- spzx-service/service-cart/src/main/java/com/atguigu/spzx/cart/controller/CartController.java
+++ spzx-service/service-cart/src/main/java/com/atguigu/spzx/cart/controller/CartController.java
@@ -1,16 +1,16 @@
 package com.atguigu.spzx.cart.controller;
 
 import com.atguigu.spzx.cart.service.CartService;
+import com.atguigu.spzx.model.entity.h5.CartInfo;
 import com.atguigu.spzx.model.vo.common.Result;
 import com.atguigu.spzx.model.vo.common.ResultCodeEnum;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "购物车接口")
 @RestController
@@ -28,4 +28,45 @@ public class CartController {
         cartService.addToCart(skuId, skuNum);
         return Result.build(null, ResultCodeEnum.SUCCESS);
     }
+
+    @Operation(summary = "查询购物车")
+    @GetMapping("/auth/cartList")
+    public Result<List<CartInfo>> cartList() {
+        List<CartInfo> cartInfoList = cartService.getCartList();
+        return Result.build(cartInfoList, ResultCodeEnum.SUCCESS);
+    }
+
+    @Operation(summary = "删除购物车")
+    @DeleteMapping("/auth/deleteCart/{skuId}")
+    public Result deleteCart(@Parameter(name = "skuId", description = "商品skuId", required = true)
+                             @PathVariable Long skuId) {
+        cartService.deleteCart(skuId);
+        return Result.build(null, ResultCodeEnum.SUCCESS);
+    }
+
+    @Operation(summary = "更新选中商品状态")
+    @GetMapping("/auth/checkCart/{skuId}/{isChecked}")
+    public Result checkCart(@Parameter(name = "skuId", description = "商品skuId", required = true)
+                            @PathVariable(value = "skuId") Long skuId,
+                            @Parameter(name = "isChecked", description = "是否选中 1:选中 0:取消选中", required = true)
+                            @PathVariable(value = "isChecked") Integer isChecked) {
+        cartService.checkCart(skuId,isChecked);
+        return Result.build(null, ResultCodeEnum.SUCCESS);
+    }
+
+    @Operation(summary = "购物车商品的全选")
+    @GetMapping("/auth/allCheckCart/{isChecked}")
+    public Result allCheckCart(@Parameter(name = "isChecked", description = "是否选中 1:选中 0:取消选中", required = true)
+                                   @PathVariable(value = "isChecked") Integer isChecked) {
+        cartService.allCheckCart(isChecked);
+        return Result.build(null, ResultCodeEnum.SUCCESS);
+    }
+
+    @Operation(summary="清空购物车")
+    @GetMapping("/auth/clearCart")
+    public Result clearCart(){
+        cartService.clearCart();
+        return Result.build(null, ResultCodeEnum.SUCCESS);
+    }
+
 }
--- spzx-service/service-cart/src/main/java/com/atguigu/spzx/cart/service/impl/CartServiceImpl.java
+++ spzx-service/service-cart/src/main/java/com/atguigu/spzx/cart/service/impl/CartServiceImpl.java
@@ -9,8 +9,12 @@ import com.atguigu.spzx.utils.AuthContextUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CartServiceImpl implements CartService {
@@ -26,20 +30,19 @@ public class CartServiceImpl implements CartService {
     @Override
     public void addToCart(Long skuId, Integer skuNum) {
         // 获取当前登录用户的id 从ThreadLocal中
-//        redisTemplate.opsForValue().get("user:cart");
 
-        CartInfo cartInfo=null;
+        CartInfo cartInfo = null;
         Long userId = AuthContextUtil.getUserInfo().getId();
         String cartKey = getCartKey(userId);
 
 
         //先获取redis中存入的数据中的skuId的数据 key是使用getCartKey定义好的值
-        Object cartInfoObj = redisTemplate.opsForHash().get(cartKey, skuId);
+        Object cartInfoObj = redisTemplate.opsForHash().get(cartKey, skuId.toString());
         // 当购物车中没用该商品的时候，则直接添加到购物车！
         if (cartInfoObj != null) {
             //将json解析为java对象 反序列化 将redis拿到的数据转化为java对象
             cartInfo = JSON.parseObject(cartInfoObj.toString(), CartInfo.class);
-            cartInfo.setSkuNum(cartInfo.getSkuNum() + 1);//点击后商品数量增加
+            cartInfo.setSkuNum(cartInfo.getSkuNum() + skuNum);//点击后商品数量增加
             cartInfo.setIsChecked(1);
             cartInfo.setUpdateTime(new Date());
         } else {//购物车没有该商品就添加新的商品
@@ -59,9 +62,90 @@ public class CartServiceImpl implements CartService {
         }
 
         //将cartInfo的信息存入redis
-        redisTemplate.opsForHash().put(cartKey, skuId, JSON.toJSONString(cartInfo));
+        redisTemplate.opsForHash().put(cartKey, skuId.toString(), JSON.toJSONString(cartInfo));
     }
 
+    @Override
+    public List<CartInfo> getCartList() {
+        Long userId = AuthContextUtil.getUserInfo().getId();
+        String cartKey = getCartKey(userId);
+
+        //从redis中取出数据
+        List<Object> cartInfoList = redisTemplate.opsForHash().values(cartKey);
+
+        //获取value的json字符串列表
+        if (!CollectionUtils.isEmpty(cartInfoList)) {
+            //进行计算或者转换 转换为CartInfo类型对象
+            //按照添加时间排序
+
+            List<CartInfo> infoList = cartInfoList.stream()
+                    .map(cartInfoObj -> JSON.parseObject(cartInfoObj.toString(), CartInfo.class))
+                    .sorted((cartInfo1, cartInfo2) -> cartInfo1.getCreateTime().compareTo(cartInfo2.getCreateTime()))
+                    .collect(Collectors.toList());
+
+            return infoList;
+        }
+        //否则返回空数组
+        return new ArrayList<>();
+
+    }
+
+    @Override
+    public void deleteCart(Long skuId) {
+        Long userId = AuthContextUtil.getUserInfo().getId();
+        String cartKey = getCartKey(userId);
+
+        //从redis中删除相关内容
+        redisTemplate.opsForHash().delete(cartKey, skuId.toString());
+
+    }
+
+    @Override
+    public void checkCart(Long skuId, Integer isChecked) {
+        Long userId = AuthContextUtil.getUserInfo().getId();
+        String cartKey = getCartKey(userId);
+
+
+        Boolean hasKey = redisTemplate.opsForHash().hasKey(cartKey, skuId.toString());
+        if (hasKey) {
+            //获取redis中的数据
+            String cartInfoJSON = redisTemplate.opsForHash().get(cartKey, skuId.toString()).toString();
+            //将json解析为java对象 反序列化 将redis拿到的数据转化为java对象
+            CartInfo cartInfo = JSON.parseObject(cartInfoJSON.toString(), CartInfo.class);
+            cartInfo.setIsChecked(isChecked);
+            cartInfo.setUpdateTime(new Date());
+            //将cartInfo的信息存入redis
+            redisTemplate.opsForHash().put(cartKey, skuId.toString(), JSON.toJSONString(cartInfo));
+        }
+    }
+
+    @Override
+    public void allCheckCart(Integer isChecked) {
+        Long userId = AuthContextUtil.getUserInfo().getId();
+        String cartKey = getCartKey(userId);
+
+        List<Object> cartInfoList = redisTemplate.opsForHash().values(cartKey);
+        if (!CollectionUtils.isEmpty(cartInfoList)) {
+            //进行计算或者转换 转换为CartInfo类型对象
+            cartInfoList.stream().map(cartInfoObj -> {
+                CartInfo cartInfo = JSON.parseObject(cartInfoObj.toString(), CartInfo.class);
+                cartInfo.setIsChecked(isChecked);
+                //将cartInfo的信息存入redis
+                return cartInfo;
+            }).forEach(cartInfo -> redisTemplate.opsForHash()
+                    .put(cartKey, cartInfo.getSkuId().toString(), JSON.toJSONString(cartInfo)));
+        }
+    }
+
+    @Override
+    public void clearCart() {
+        Long userId = AuthContextUtil.getUserInfo().getId();
+        String cartKey = getCartKey(userId);
+        redisTemplate.delete(cartKey);
+    }
+
+
+    //redis格式的转换
     private String getCartKey(Long userId) {
         //转为redis的格式
         return "user:cart:" + userId;
